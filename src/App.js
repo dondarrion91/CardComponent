b355786f56bd1ce477b52d6e94d634790191e046
julian <you@example.com>
2020-07-22 16:18:23 -0300

    ADD: Readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment,useState} from 'react';
+
+// components
+import Card from './components/Card';
 
 function App() {
+
+  // state de la tarjeta 
+  const [contenido, setcontenido] = useState({
+    title: 'Titulo',
+    body: 'Cuerpo de la tarjeta'
+  });
+
+  // funcion onChange title
+  const onChangeTitle = e => {
+    setcontenido({
+      ...contenido,
+      title: e.target.value
+    });    
+  }
+
+
+  // funcion onChange title
+  const onChangeBody = e => {
+    setcontenido({
+      ...contenido,
+      body: e.target.value
+    });    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>  
+      <div className="container text-white mt-5">
+        <form className="col-12 col-lg-6 mx-auto">
+          <h4>Ingrese un titulo y un cuerpo para la tarjeta</h4>
+          <div className="form-group">
+            <label htmlFor="title">title</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="title" 
+              aria-describedby="title" 
+              placeholder="title"
+              onChange={onChangeTitle}
+            />          
+          </div>
+          <div className="form-group">
+            <label htmlFor="body">body</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="body" 
+              placeholder="body"   
+              onChange={onChangeBody}         
+            />
+          </div>              
+        </form>    
+
+        <h4 className="text-center">Componente Card</h4>
+         
+         
+         
+         {/*Componente Card  */}        
+        <Card
+          contenido={contenido}
+        />
+
+
+      </div>             
+    </Fragment>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+// styled components
+import styled from '@emotion/styled';
+
+const CardContainer = styled.div`
+    margin: auto;
+    margin-bottom: 20px;
+    height: 300px;
+    width: 150px;    
+    font-family:Arial, Helvetica, sans-serif;
+    background: #CCCCCC;
+`
+
+const CardHeader = styled.div`
+    background: #FFFFFF;    
+    color: #000;   
+    padding: 0px 10px;
+    display: flex;
+    align-items: center;
+    height:25%;
+    h2{
+        font-size:100%;
+        margin:0;
+        word-break: break-all;
+    }
+`
+
+const CardBody = styled.div`  
+    height:75%;    
+    display: flex;
+    align-items: center;
+    p{
+        font-size: 14px;
+        text-align: center;
+        padding: 0px 10px        
+    }
+`
+
+const Card = ({contenido}) => {
+    
+    return ( 
+        <CardContainer>
+            <CardHeader>
+                <h2>{contenido.title}</h2>                
+            </CardHeader>
+            <CardBody>
+                <p>{contenido.body}</p>                
+            </CardBody>
+        </CardContainer>
+     );
+}
+ 
+export default Card;
